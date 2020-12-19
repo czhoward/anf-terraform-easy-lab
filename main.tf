7cd817701179a2ea7490786d7f3e8ff8c3239222
Colin Howard <76244495+czhoward@users.noreply.github.com>
2020-12-19 16:24:41 +0000

    Update main.tf
    take out originators personalisation and the netapp stuff as not using that just yet - messed about with the AD domain bit basically played in order to get a feel for github and for terraform

--- main.tf
+++ main.tf
@@ -14,45 +14,31 @@ provider "azurerm" {
 
 # Core Azure resources/configuration
 
-resource "azurerm_resource_group" "tf-kirkr-group" {
-  name     = "tf-kirkr-group"
+resource "azurerm_resource_group" "tf-group" {
+  name     = "tf-group"
   location = var.region
 }
 
-resource "azurerm_virtual_network" "tf-kirkr-vnet" {
-  name                = "tf-kirkr-vnet"
-  resource_group_name = azurerm_resource_group.tf-kirkr-group.name
-  location            = azurerm_resource_group.tf-kirkr-group.location
+resource "azurerm_virtual_network" "tf-vnet" {
+  name                = "tf-vnet"
+  resource_group_name = azurerm_resource_group.tf-group.name
+  location            = azurerm_resource_group.tf-group.location
   address_space       = ["20.0.0.0/16"]
 }
 
+# subnet for VMs
 resource "azurerm_subnet" "vms" {
   name                 = "vms"
-  resource_group_name  = azurerm_resource_group.tf-kirkr-group.name
-  virtual_network_name = azurerm_virtual_network.tf-kirkr-vnet.name
+  resource_group_name  = azurerm_resource_group.tf-group.name
+  virtual_network_name = azurerm_virtual_network.tf-vnet.name
   address_prefixes     = ["20.0.1.0/24"]
 }
 
-resource "azurerm_subnet" "anf" {
-  name                 = "anf"
-  resource_group_name  = azurerm_resource_group.tf-kirkr-group.name
-  virtual_network_name = azurerm_virtual_network.tf-kirkr-vnet.name
-  address_prefixes     = ["20.0.2.0/24"]
-
-  delegation {
-    name = "anfdelegation"
-
-    service_delegation {
-      name = "Microsoft.Netapp/volumes"
-    }
-  }
-}
-
 # Provision public IP for the Windows AD server
-resource "azurerm_public_ip" "tf-kirkr-win-ad-01-public-ip" {
-  name                = "tf-kirkr-linux-win-ad-01-public-ip"
-  location            = azurerm_resource_group.tf-kirkr-group.location
-  resource_group_name = azurerm_resource_group.tf-kirkr-group.name
+resource "azurerm_public_ip" "tf-win-ad-01-public-ip" {
+  name                = "tf-linux-win-ad-01-public-ip"
+  location            = azurerm_resource_group.tf-group.location
+  resource_group_name = azurerm_resource_group.tf-group.name
   allocation_method   = "Dynamic"
 
   tags = {
@@ -61,10 +47,10 @@ resource "azurerm_public_ip" "tf-kirkr-win-ad-01-public-ip" {
 }
 
 # Provision and configure the NIC interface for the Windows AD server
-resource "azurerm_network_interface" "tf-kirk-win-ad-01-nic-01" {
-  name                          = "tf-kirk-win-ad-01-nic-01"
-  location                      = azurerm_resource_group.tf-kirkr-group.location
-  resource_group_name           = azurerm_resource_group.tf-kirkr-group.name
+resource "azurerm_network_interface" "tf-win-ad-01-nic-01" {
+  name                          = "tf-win-ad-01-nic-01"
+  location                      = azurerm_resource_group.tf-group.location
+  resource_group_name           = azurerm_resource_group.tf-group.name
   enable_accelerated_networking = true
 
   ip_configuration {
@@ -72,15 +58,15 @@ resource "azurerm_network_interface" "tf-kirk-win-ad-01-nic-01" {
     subnet_id                     = azurerm_subnet.vms.id
     private_ip_address_allocation = "Static"
     private_ip_address            = "20.0.1.100"
-    public_ip_address_id          = azurerm_public_ip.tf-kirkr-win-ad-01-public-ip.id
+    public_ip_address_id          = azurerm_public_ip.tf-win-ad-01-public-ip.id
   }
 }
 
 # Create the NSG for the Windows AD server
-resource "azurerm_network_security_group" "tf-kirkr-win-ad-01-nsg" {
-  name                = "tf-kirkr-win-ad-01-nsg"
-  location            = azurerm_resource_group.tf-kirkr-group.location
-  resource_group_name = azurerm_resource_group.tf-kirkr-group.name
+resource "azurerm_network_security_group" "tf-win-ad-01-nsg" {
+  name                = "tf-win-ad-01-nsg"
+  location            = azurerm_resource_group.tf-group.location
+  resource_group_name = azurerm_resource_group.tf-group.name
 
   security_rule {
     name                       = "RDP"
@@ -100,15 +86,15 @@ resource "azurerm_network_security_group" "tf-kirkr-win-ad-01-nsg" {
 }
 
 # Provision the Windows AD server VM
-resource "azurerm_windows_virtual_machine" "tf-kirkr-win-ad-01" {
+resource "azurerm_windows_virtual_machine" "tf-win-ad-01" {
   name                = "ad01"
-  resource_group_name = azurerm_resource_group.tf-kirkr-group.name
-  location            = azurerm_resource_group.tf-kirkr-group.location
+  resource_group_name = azurerm_resource_group.tf-group.name
+  location            = azurerm_resource_group.tf-group.location
   size                = "Standard_D4s_v3"
   admin_username      = var.username
   admin_password      = var.password
   network_interface_ids = [
-    azurerm_network_interface.tf-kirk-win-ad-01-nic-01.id,
+    azurerm_network_interface.tf-win-ad-01-nic-01.id,
   ]
 
   os_disk {
@@ -124,115 +110,55 @@ resource "azurerm_windows_virtual_machine" "tf-kirkr-win-ad-01" {
   }
 }
 
-# TODO output "azurerm_windows_virtual_machine" { value = azurerm_windows_virtual_machine.tf-kirkr-win-ad-01.private_key_pem }
-
-# Connect the security group to the network interface (Enable RDP connectivity)
-resource "azurerm_network_interface_security_group_association" "tf-kirkr-nsg-asso-02" {
-  network_interface_id      = azurerm_network_interface.tf-kirk-win-ad-01-nic-01.id
-  network_security_group_id = azurerm_network_security_group.tf-kirkr-win-ad-01-nsg.id
+# Connect the security group to the network interface (Enable onnectivity)
+resource "azurerm_network_interface_security_group_association" "tf-nsg-asso-02" {
+  network_interface_id      = azurerm_network_interface.tf-win-ad-01-nic-01.id
+  network_security_group_id = azurerm_network_security_group.tf-win-ad-01-nsg.id
 }
 
-# TODO - Configure AD role and services
+# modified from
+# https://registry.terraform.io/modules/ghostinthewires/promote-dc/azurerm/latest?tab=resources
+# https://github.com/ghostinthewires/terraform-azurerm-promote-dc/blob/master/main.tf
 // the `exit_code_hack` is to keep the VM Extension resource happy
-# locals { 
-#   import_command       = "Import-Module ADDSDeployment"
-#   password_command     = "$password = ConvertTo-SecureString ${var.admin_password} -AsPlainText -Force"
-#   install_ad_command   = "Add-WindowsFeature -name ad-domain-services -IncludeManagementTools"
-#   configure_ad_command = "Install-ADDSForest -CreateDnsDelegation:$false -DomainMode Win2012R2 -DomainName ${var.active_directory_domain} -DomainNetbiosName ${var.active_directory_netbios_name} -ForestMode Win2012R2 -InstallDns:$true -SafeModeAdministratorPassword $password -Force:$true"
-#   shutdown_command     = "shutdown -r -t 10"
-#   exit_code_hack       = "exit 0"
-#   powershell_command   = "${local.import_command}; ${local.password_command}; ${local.install_ad_command}; ${local.configure_ad_command}; ${local.shutdown_command}; ${local.exit_code_hack}"
-# }
+locals { 
+  import_command       = "Import-Module ADDSDeployment, ActiveDirectory"
+  password_command     = "$password = ConvertTo-SecureString ${var.password} -AsPlainText -Force"
+  install_ad_command   = "Add-WindowsFeature -name ad-domain-services -IncludeManagementTools"
+  configure_ad_command = "Install-ADDSForest -CreateDnsDelegation:$false -DomainMode Win2012R2 -DomainName ${var.domainname} -DomainNetbiosName ${var.adnetbiosname} -ForestMode Win2012R2 -InstallDns:$true -SafeModeAdministratorPassword $password -Force:$true"
+  shutdown_command     = "shutdown -r -t 10"
+  exit_code_hack       = "exit 0"
+  powershell_command   = "${local.import_command}; ${local.password_command}; ${local.install_ad_command}; ${local.configure_ad_command}; ${local.shutdown_command}; ${local.exit_code_hack}"
+}
 
-resource "azurerm_virtual_machine_extension" "tf-kirkr-win-ad-01-ext-install-ad" {
-  name                 = azurerm_windows_virtual_machine.tf-kirkr-win-ad-01.name
-  virtual_machine_id   = azurerm_windows_virtual_machine.tf-kirkr-win-ad-01.id
+resource "azurerm_virtual_machine_extension" "create-active-directory-forest" {
+  name                 = azurerm_windows_virtual_machine.tf-win-ad-01.name
+  virtual_machine_id   = azurerm_windows_virtual_machine.tf-win-ad-01.id
+
   publisher            = "Microsoft.Compute"
   type                 = "CustomScriptExtension"
   type_handler_version = "1.10"
 
-  protected_settings = <<PROTECTED_SETTINGS
+  settings = <<SETTINGS
     {
-        "commandToExecute": "powershell.exe -Command \"Import-Module ADDSDeployment, ActiveDirectory; $password = ConvertTo-SecureString ${var.password} -AsPlainText -Force; Add-WindowsFeature -name ad-domain-services -IncludeManagementTools; Install-ADDSForest -DomainName ${var.domainname} -SafeModeAdministratorPassword $password -Force:$true; shutdown -r -t 10; exit 0\""
+        "commandToExecute": "powershell.exe -Command \"${local.powershell_command}\""
     }
-PROTECTED_SETTINGS
+SETTINGS
 
   tags = {
-    environment = "Production"
+    environment = "Terraform Demo"
   }
 
   depends_on = [
-    azurerm_windows_virtual_machine.tf-kirkr-win-ad-01
+    azurerm_windows_virtual_machine.tf-win-ad-01
   ]
-}
-
-# Provision NetApp Account
-resource "azurerm_netapp_account" "tf-kirkr-anf" {
-  name                = "tf-kirkr-anf"
-  resource_group_name = azurerm_resource_group.tf-kirkr-group.name
-  location            = azurerm_resource_group.tf-kirkr-group.location
-
-  # Disabled due to test lab already having an AD:
-  # Error: Error waiting for creation of NetApp Account "tf-kirkr-anf" (Resource Group "tf-kirkr-group"): Code="BadRequest" Message="Only one active directory allowed within the same region. Account core-west-europe in resource group emea-core-west-europe-anf currently has an active directory connection string." Details=[{"code":"TooManyActiveDirectories","message":"Only one active directory allowed within the same region. Account core-west-europe in resource group emea-core-west-europe-anf currently has an active directory connection string."}]
-
-  #   active_directory {
-  #   username            = var.username
-  #   password            = var.password
-  #   smb_server_name     = var.smbservername
-  #   dns_servers         = ["20.0.1.100"]
-  #   domain              = var.domainname
-  # }
-
-  depends_on = [
-    azurerm_windows_virtual_machine.tf-kirkr-win-ad-01
-  ]
-
-}
-
-# Provision Storage Pool
-resource "azurerm_netapp_pool" "tf-kirkr-anf-pool-01" {
-  name                = "tf-kirkr-anf-pool-01"
-  account_name        = azurerm_netapp_account.tf-kirkr-anf.name
-  location            = azurerm_resource_group.tf-kirkr-group.location
-  resource_group_name = azurerm_resource_group.tf-kirkr-group.name
-  service_level       = "Standard"
-  size_in_tb          = 4
-
-  depends_on = [
-    azurerm_netapp_account.tf-kirkr-anf,
-  ]
-}
-
-# Provision NFS Volume
-
-resource "azurerm_netapp_volume" "tf-kirkr-vol-01" {
-  #   lifecycle {
-  #     prevent_destroy = true
-  #   }
-
-  name                = "tf-kirkr-vol-01"
-  location            = azurerm_resource_group.tf-kirkr-group.location
-  resource_group_name = azurerm_resource_group.tf-kirkr-group.name
-  account_name        = azurerm_netapp_account.tf-kirkr-anf.name
-  pool_name           = azurerm_netapp_pool.tf-kirkr-anf-pool-01.name
-  volume_path         = "nfs01"
-  service_level       = "Standard"
-  subnet_id           = azurerm_subnet.anf.id
-  protocols           = ["NFSv3"]
-  storage_quota_in_gb = 1000
-
-  depends_on = [
-    azurerm_netapp_pool.tf-kirkr-anf-pool-01,
-  ]
-
 }
 
 # Create a Public IP address for the network
 
-resource "azurerm_public_ip" "tf-kirkr-linux-vm-01-public-ip" {
-  name                = "tf-kirkr-linux-vm-01-public-ip"
-  location            = azurerm_resource_group.tf-kirkr-group.location
-  resource_group_name = azurerm_resource_group.tf-kirkr-group.name
+resource "azurerm_public_ip" "tf-linux-vm-01-public-ip" {
+  name                = "tf-linux-vm-01-public-ip"
+  location            = azurerm_resource_group.tf-group.location
+  resource_group_name = azurerm_resource_group.tf-group.name
   allocation_method   = "Dynamic"
 
   tags = {
@@ -240,12 +166,13 @@ resource "azurerm_public_ip" "tf-kirkr-linux-vm-01-public-ip" {
   }
 }
 
+
 # Create an NSG for SSH to the Linux VM
 
-resource "azurerm_network_security_group" "tf-kirkr-linux-vm-01-nsg" {
-  name                = "mtf-kirkr-linux-vm-01-nsg"
-  location            = azurerm_resource_group.tf-kirkr-group.location
-  resource_group_name = azurerm_resource_group.tf-kirkr-group.name
+resource "azurerm_network_security_group" "tf-linux-vm-01-nsg" {
+  name                = "tf-linux-vm-01-nsg"
+  location            = azurerm_resource_group.tf-group.location
+  resource_group_name = azurerm_resource_group.tf-group.name
 
   security_rule {
     name                       = "SSH"
@@ -266,17 +193,17 @@ resource "azurerm_network_security_group" "tf-kirkr-linux-vm-01-nsg" {
 
 # Create Network Interface for Linux VM
 
-resource "azurerm_network_interface" "tf-kirkr-linux-vm-nic-01" {
-  name                          = "tf-kirkr-linux-vm-nic-01"
-  location                      = azurerm_resource_group.tf-kirkr-group.location
-  resource_group_name           = azurerm_resource_group.tf-kirkr-group.name
+resource "azurerm_network_interface" "tf-linux-vm-nic-01" {
+  name                          = "tf-linux-vm-nic-01"
+  location                      = azurerm_resource_group.tf-group.location
+  resource_group_name           = azurerm_resource_group.tf-group.name
   enable_accelerated_networking = true
 
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.vms.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.tf-kirkr-linux-vm-01-public-ip.id
+    public_ip_address_id          = azurerm_public_ip.tf-linux-vm-01-public-ip.id
   }
 
   tags = {
@@ -285,31 +212,31 @@ resource "azurerm_network_interface" "tf-kirkr-linux-vm-nic-01" {
 }
 
 # Connect the security group to the network interface
-resource "azurerm_network_interface_security_group_association" "tf-kirkr-nsg-asso-01" {
-  network_interface_id      = azurerm_network_interface.tf-kirkr-linux-vm-nic-01.id
-  network_security_group_id = azurerm_network_security_group.tf-kirkr-linux-vm-01-nsg.id
+resource "azurerm_network_interface_security_group_association" "tf-nsg-asso-01" {
+  network_interface_id      = azurerm_network_interface.tf-linux-vm-nic-01.id
+  network_security_group_id = azurerm_network_security_group.tf-linux-vm-01-nsg.id
 }
 
 # Create (and display) an SSH key
-resource "tls_private_key" "netapp_ssh" {
+resource "tls_private_key" "tf_ssh" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
-output "tls_private_key" { value = tls_private_key.netapp_ssh.private_key_pem }
+output "tls_private_key" { value = tls_private_key.tf_ssh.private_key_pem }
 
 # Create the Virtual Machine
-resource "azurerm_linux_virtual_machine" "tf-kirkr-linux-vm-01" {
-  name                            = "tf-kirkr-linux-vm-01"
-  resource_group_name             = azurerm_resource_group.tf-kirkr-group.name
-  location                        = azurerm_resource_group.tf-kirkr-group.location
+resource "azurerm_linux_virtual_machine" "tf-linux-vm-01" {
+  name                            = "tf-linux-vm-01"
+  resource_group_name             = azurerm_resource_group.tf-group.name
+  location                        = azurerm_resource_group.tf-group.location
   size                            = "Standard_D4s_v3"
   admin_username                  = var.username
   disable_password_authentication = true
-  network_interface_ids           = [azurerm_network_interface.tf-kirkr-linux-vm-nic-01.id]
+  network_interface_ids           = [azurerm_network_interface.tf-linux-vm-nic-01.id]
 
   admin_ssh_key {
-    username   = "netapp"
-    public_key = tls_private_key.netapp_ssh.public_key_openssh
+    username   = "tf"
+    public_key = tls_private_key.tf_ssh.public_key_openssh
   }
 
   os_disk {
